bfe84ed1219ef50cfa136fec9df8dc8cd10a20d0
unknown <925448163@qq.com>
2018-07-21 21:41:10 +0800

    Branch

--- src/js/Branch.js
+++ src/js/Branch.js
@@ -2,13 +2,13 @@ export {Branch}
 /**
  * Branch类
  * @constructs
- * @param {float} x x-coordinate
- * @param {float} y y-coordinate
- * @param {float} radius The radius of a point
- * @param {float} angle The angle of deflection
- * @param {float} speed The speed of growth
- * @param {int} generation The generation of the branch
- * @param {float} distance The distance of a new point and an old point 
+ * @param {number} x x-coordinate
+ * @param {number} y y-coordinate
+ * @param {number} radius The radius of a point
+ * @param {number} angle The angle of deflection
+ * @param {number} speed The speed of growth
+ * @param {number} generation The generation of the branch
+ * @param {number} distance The distance of a new point and an old point 
  */
 function Branch() {
   this.x = canvas.width / 2
@@ -32,17 +32,77 @@ Branch.prototype = {
     this.split()
     this.die()
   },
+  /**
+   * Draw a branch point
+   */
   draw: function() {
     ctx.fillStyle = "#946A2C"
-    ctx.shadowcolor = "#000000"
-    ctx.shadowBlur = 2
+    ctx.shadowcolor = "#946A2C"
     cts.beginPath()
     ctx.moveTo(this.x, this.y)
     ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, true)
     ctx.closePath()
     ctx.fill()
   },
+  /**
+   * Draw a leaf point and record the point
+   */
   drawPoints: function() {
-
+    let p = Math.random()
+    let theta = Math.PI / 2
+    ctx.fillStyle = "#83AD50"
+    ctx.shadowColor = "#83AD50"
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.arc(this.x + 10 * p * Math.cos(theta * p), this.y - 10 * p * Math.sin(theta * p), 4, 0, 2 * Math.PI, true)
+    ctx.closePath()
+    ctx.fill()
+  },
+  /**
+   * Generate new a branch point and record the point
+   */
+  iterate: function() {
+    let deltaX = this.speed * Math.cos(this.angle)
+    let deltaY = - this.speed * Math.sin(this.angle)
+    this.x += deltaX
+    this.y += deltaY
+    this.radius *= (0.99 - this.generation / 250)
+    let deltaDistance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))
+    this.distance += deltaDistance
+    if (this.speed > this.radius * 2) {
+      this.speed = this.radius * 2
+    }
+    this.angle += Math.random() / 5 - 1 / 5 / 2
+  },
+  /**
+   * Generate a new branch
+   */
+  split: function() {
+    let splitChance = 0
+    if (this.generation === 1)
+      splitChance = this.distance / height - 0.2
+    else if (this.generation < 3)
+      splitChance = this.distance / height - 0.1
+    if (Math.random() < splitChance) {
+      let n = 2 + Math.round(Math.random() * 3)
+      for (var i = 0; i < n; i++) {
+        let branch = new Branch()
+        branch.x = this.x
+        branch.y = this.y
+        branch.angle = this.angle
+        branch.radius = this.radius * 0.9
+        branch.generation++
+        branches.add(branch)
+      }
+      branches.remove(this)
+    }
+  },
+  /**
+   * End the growth of the tree
+   */
+  die: function() {
+    if (this.radius < 0.5) {
+      branches.remove(this)
+    }
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import {createCanvas} from "./canvas.js"
 window.addEventListener("load", main)
 let canvas = document.createElement("canvas")
 let ctx = canvas.getContext("2d")
+ctx.shadowBlur = 2
 
 /**
  * Executive function
