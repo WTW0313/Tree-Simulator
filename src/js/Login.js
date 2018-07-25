8664d4bd398336ecfbdb073fe1009b5ef36a80ec
SingularD <906229740@qq.com>
2018-07-25 21:32:14 +0800

    ui

--- src/js/Login.js
+++ src/js/Login.js
@@ -1,13 +1,17 @@
+import {Users} from "./Users"
 $(function () {
-    $("#btn").click(function () {
+    $("#login").click(function () {
         $.post("../php/Login.php",{
             username:document.getElementById("username").value,
             password:document.getElementById("password").value
             },function (data) {
                 if (data === "登录成功！") {
-                    console.log(data);
+                    const currentUser = new Users(document.getElementById("username").value,null);
+                    // location.href = "Menu.html";
                 }else alert(data);
             }
             )
     })
+
 })
+
